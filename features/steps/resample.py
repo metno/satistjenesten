151a3377972121f48b340990230185735b448f78
Mikhail Itkin <itkin.m@gmail.com>
2014-11-04 13:59:29 +0000

    [behave] basic design for the avhrr resampler

--- features/steps/resample.py
+++ features/steps/resample.py
@@ -1,21 +1,26 @@
 import os
+import satistjenesten
 
 @given(u'we process file {netcdf_filepath}')
 def step_impl(context, netcdf_filepath):
 	context.netcdf_filepath = netcdf_filepath
-	assert True
 
 @then(u'load the file using yaml config {avhrr_l1b_yaml_config}')
 def step_impl(context, avhrr_l1b_yaml_config):
 	context.avhrr_l1b_yaml_config = avhrr_l1b_yaml_config
-	assert True
+	context.avhrr_scene = satistjenesten.data.SatScene()
+	context.avhrr_scene.config_file = avhrr_l1b_yaml_config
+	context.avhrr_scene.load()
+	assert False
 
 @then(u'resample it to the istjenesten generic area {area_name}')
 def step_impl(context, area_name):
-	context.area_name = area_name
-	assert True
+	context.avhrr_scene.area_name = area_name
+	context.avhrr_scene.resample()
+	assert False
 
 @then(u'export it as a netcdf file {output_file}')
 def step_impl(context, output_file):
 	context.output_file = output_file
-	assert True
+	context.avhrr_scene.write_netcdf(output_file)
+	assert False
