ab0bb1aaf5cffb498c5b5ad1f9a8e1f985e0b1b4
Mikhail Itkin <itkin.m@gmail.com>
2014-12-04 14:58:21 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 from setuptools import setup
 
 requirements = ['sphinxcontrib-napoleon']
+requires = ['numpy', 'pyresample', 'netCDF4']
 setup(name='satistjenesten',
         packages=['satistjenesten', 'tests'],
         package_dir={'satistjenesten': 'satistjenesten'},
         author='Mikhail Itkin',
         install_requires=requirements,
         description='resampler and netcdf IO')
+extras_require = {'nc': 'netCDF4'}
