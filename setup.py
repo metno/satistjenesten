4c374b6f74b376a8ca412b7f050de81017f4f000
Mikhail Itkin <itkin.m@gmail.com>
2014-12-16 11:20:17 +0100

    [setup] simplified requirements specifications
    * all the environment stuff including tests goes to requirements.txt
    * core stuff stays in setup.py
    * split pip call in two in travis.yml

--- setup.py
+++ setup.py
@@ -1,14 +1,10 @@
 from setuptools import setup
 from setuptools import find_packages
-from setuptools_behave import behave_test
 
 requirements = ['numpy',
                 'netCDF4',
                 'pyresample',
                 'pyyaml']
-
-test_requirements = ['nose',
-                     'behave>=1.2.4']
 
 readme_contents = open('README.md', 'r').read()
 
@@ -18,7 +14,5 @@ setup(
       description='Melt ponds fraction retrieval',
       long_description=readme_contents,
       install_requires=requirements,
-      tests_require=test_requirements,
       test_suite='tests',
-      cmdclass={'behave_test': behave_test,}
       )
