43ff8d3048975e18b41f65c380388ad4c8682a3f
Mikhail Itkin <itkin.m@gmail.com>
2014-12-16 10:50:02 +0100

    updated setup.py (dependencies)

--- setup.py
+++ setup.py
@@ -1,12 +1,24 @@
 from setuptools import setup
+from setuptools import find_packages
+from setuptools_behave import behave_test
 
-requirements = ['sphinxcontrib-napoleon', 'mock']
-requires = ['numpy', 'pyresample', 'netCDF4']
-test_requires=["mock"]
-setup(name='satistjenesten',
-        packages=['satistjenesten', 'satistjenesten'],
-        package_dir={'satistjenesten': 'satistjenesten'},
-        author='Mikhail Itkin',
-        install_requires=requirements,
-        description='resampler and netcdf IO')
-extras_require = {'nc': 'netCDF4'}
+requirements = ['numpy',
+                'netCDF4',
+                'pyresample',
+                'pyyaml']
+
+test_requirements = ['nose',
+                     'behave>=1.2.4']
+
+readme_contents = open('README.md', 'r').read()
+
+setup(
+      name='AVHRR-meltponds',
+      author='Mikhail Itkin',
+      description='Melt ponds fraction retrieval',
+      long_description=readme_contents,
+      install_requires=requirements,
+      tests_require=test_requirements,
+      test_suite='tests',
+      cmdclass={'behave_test': behave_test,}
+      )
