41d6ca1c5911e7e65c30aa6e90daa0d6e01da08c
Mikhail Itkin <itkin.m@gmail.com>
2015-02-09 11:19:13 +0100

    [mitiff, setup] added a PIL dependency for TIFF handling

--- setup.py
+++ setup.py
@@ -4,14 +4,15 @@ from setuptools import find_packages
 requirements = ['numpy',
                 'netCDF4 == 1.1.1',
                 'pyresample',
-                'pyyaml']
+                'pyyaml',
+                'PIL']
 
 readme_contents = open('README.md', 'r').read()
 
 setup(
       name='AVHRR-meltponds',
       author='Mikhail Itkin',
-      description='Melt ponds fraction retrieval',
+      description='Istjenesten satellite processing suite',
       long_description=readme_contents,
       install_requires=requirements,
       test_suite='tests',
