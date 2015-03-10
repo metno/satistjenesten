9df37b029f2f0090c60138999b51b8bfb581487b
Mikhail Itkin <itkin.m@gmail.com>
2015-03-10 16:07:22 +0100

    [mitiff] proper tags parser and geotiff export

--- satistjenesten/io.py
+++ satistjenesten/io.py
@@ -3,8 +3,11 @@ import collections
 import numpy
 import re
 import datetime
-import pyresample
+from copy import copy, deepcopy
+from osgeo import gdal, osr
 
+from pyresample import geometry
+from pyresample import kd_tree
 from PIL import Image
 import netCDF4 as nc
 from satistjenesten import utils
@@ -18,13 +21,15 @@ class SatBand(object):
         self.latitude = None
         self.longitude = None
 
-class GenericFormat(object):
-    def __init__(self, file_path, config_path):
-        self.file_path = file_path
-        self.yaml_dict = config_path
+class GenericScene(object):
+    def __init__(self, filepath=None, configpath=None):
+        self.file_path = filepath
+        self.yaml_dict = configpath
         self.bands = None
         self.longitudes = None
         self.latitudes = None
+        self.area_def = None
+        self.timestamp = None
 
     def get_filehandle(self):
         self.filehandle = open(self.file_path, 'r')
@@ -33,7 +38,78 @@ class GenericFormat(object):
         self.get_filehandle()
         self.get_bands()
 
-class Netcdf(GenericFormat):
+    def get_coordinates(self):
+        if self.area_def is None:
+            self.get_area_def()
+        self.longitudes, self.latitudes = self.area_def.get_lonlats()
+
+    def resample_to_area(self, target_area_def):
+        """
+        Resample existing scene to the provided area definition
+
+        """
+        attributes_list_to_pass = ['bands', 'timestamp']
+        resampled_scene = GenericScene()
+        # resampled_scene.bands = deepcopy(self.bands)
+        resampled_scene.area_def = target_area_def
+        copy_attributes(self, resampled_scene, attributes_list_to_pass)
+
+        try:
+            self.area_def = geometry.SwathDefinition(lons=self.longitudes, lats=self.latitudes)
+        except:
+            self.get_area_def()
+
+        valid_input_index, valid_output_index, index_array, distance_array = \
+                kd_tree.get_neighbour_info(self.area_def, resampled_scene.area_def,
+                                            self.area_def.pixel_size_x*2.5, neighbours = 1)
+
+        bands_number = len(resampled_scene.bands)
+        for i, band in enumerate(resampled_scene.bands.values()):
+            print "Resampling band {0:d}/{1:d}".format(i+1, bands_number)
+            swath_data = deepcopy(band.data)
+            band.data = kd_tree.get_sample_from_neighbour_info('nn', resampled_scene.area_def.shape,
+                                                                swath_data,
+                                                                valid_input_index,
+                                                                valid_output_index,
+                                                                index_array)
+        return resampled_scene
+
+    def save_geotiff(self, filepath):
+        """
+        Export Scene in GeoTIFF format
+        """
+        self.export_path = filepath
+        gtiff_driver = gdal.GetDriverByName('GTiff')
+        gtiff_format = gdal.GDT_Byte
+        gtiff_options = []
+        bands_number = len(self.bands)
+        gtiff_dataset = gtiff_driver.Create(self.export_path,
+                                             int(self.area_def.x_size),
+                                             int(self.area_def.y_size),
+                                             bands_number,
+                                             gtiff_format,
+                                             gtiff_options)
+
+        geometry_list = (self.area_def.area_extent[0],
+                         self.area_def.pixel_size_x,
+                         0,
+                         self.area_def.area_extent[3],
+                         0,
+                         self.area_def.pixel_size_y * -1)
+
+        gtiff_dataset.SetGeoTransform(geometry_list)
+        srs = osr.SpatialReference()
+        srs.ImportFromProj4(self.area_def.proj4_string)
+        gtiff_dataset.SetProjection(srs.ExportToWkt())
+
+        for i, band in enumerate(self.bands.values()):
+            raster_array = band.data
+            gtiff_dataset.GetRasterBand(i + 1).WriteArray(raster_array.astype('byte'))                    
+
+        gtiff_dataset = None
+
+
+class NetcdfScene(GenericScene):
     def get_filehandle(self):
         self.filehandle = nc.Dataset(self.file_path, 'r')
 
@@ -48,8 +124,7 @@ class Netcdf(GenericFormat):
             bands[band_name] = sat_band
         self.bands = bands
 
-
-class Mitiff(GenericFormat):
+class MitiffScene(GenericScene):
     def get_filehandle(self):
         self.filehandle = Image.open(self.file_path)
 
@@ -82,7 +157,6 @@ class Mitiff(GenericFormat):
         r = regex.search(tags_string)
         return r.groups()[0]
 
-
     def parse_mitiff_tags(self):
         tags_dict = {}
 
@@ -95,13 +169,10 @@ class Mitiff(GenericFormat):
         tags_dict['timestamp'] = datetime_timestamp
 
         tags_dict['proj_dict'] = {'proj': 'stere',
-                                  'lat_0': 90,
-                                  'lat_ts': 60,
-                                  'lon_0': 0,
-                                  'ellps': "WGS84"}
-
-        # import ipdb; ipdb.set_trace() # BREAKPOINT
-
+                                  'lat_0': '90',
+                                  'lat_ts': '60',
+                                  'lon_0': '0',
+                                  'ellps': 'WGS84'}
 
         xsize_pattern = 'Xsize:\s+(\d+)'
         xsize = self.parse_tiff_string(xsize_pattern)
@@ -160,34 +231,30 @@ class Mitiff(GenericFormat):
         y_ll_corner = y_ul_corner - y_size * y_px_size
         area_extent = (x_ll_corner, y_ll_corner, x_ur_corner, y_ur_corner)
 
-        area_def = pyresample.geometry.AreaDefinition('mitiff area',
-                                                      'dummy area name',
-                                                      'MET (diana) projection',
-                                                      proj_dict,
-                                                      x_size,
-                                                      y_size,
-                                                      area_extent)
+        area_def = geometry.AreaDefinition('mitiff area',
+                                           'dummy area name',
+                                           'MET (diana) projection',
+                                           proj_dict,
+                                           x_size,
+                                           y_size,
+                                           area_extent)
         self.area_def = area_def
-
-    def get_coordinates(self):
-        if self.area_def is None:
-            self.get_area_def()
-        self.longitudes, self.latitudes = self.area_def.get_lonlats()
 
     def load(self):
         self.get_filehandle()
         self.get_bands()
         self.get_mitiff_tags()
         self.get_area_def()
+        self.get_timestamp()
         # self.get_coordinates()
 
 def load_mitiff(file_path, config_path):
-    mitiff_scene = Mitiff(file_path, config_path)
+    mitiff_scene = MitiffScene(file_path, config_path)
     mitiff_scene.load()
     return mitiff_scene
 
 def load_netcdf(file_path, config_path):
-    netcdf_scene = Netcdf(file_path, config_path)
+    netcdf_scene = NetcdfScene(file_path, config_path)
     netcdf_scene.load()
     return netcdf_scene
 
@@ -195,3 +262,55 @@ def parse_mitiff_timestamp(string_timestamp):
     fmt = "%H:%M %d/%m-%Y"
     datetime_timestamp = datetime.datetime.strptime(string_timestamp, fmt)
     return datetime_timestamp
+
+
+def copy_attributes(object_from, object_to, attributes_list):
+    for attribute_name in attributes_list:
+        if hasattr(object_from, attribute_name):
+            the_attribute = getattr(object_from, attribute_name)
+            setattr(object_to, attribute_name, deepcopy(the_attribute))
+
+def get_area_def_from_file(area_name):
+    area_filepath = get_area_filepath()
+    return utils.load_area(area_filepath, area_name)
+
+def window_blocks(large_array, window_size):
+    """
+    Split a large 1D array into smaller non-overlapping arrays
+
+    Args:
+      large_array (numpy.ndarray): 1d array to be split in smaller blocks
+      window_size (int): window size, array shape should be divisible by this number
+
+    Returns:
+     numpy.ndarray: Resulting array with multiple small blocks of size `window_size`
+
+    """
+    y_size = large_array.shape[0]/window_size
+    blocks_array = large_array.reshape(y_size, window_size)
+    return blocks_array
+
+def rescale_lac_array_to_gac(lac_array):
+    """
+    Create a GAC AVHRR array by averaging 4 consecutive LAC pixels
+    Take only every forth scan line, omit the rest
+
+    Args:
+      lac_array (numpy.ndtype): array with scan width of 2001 pixels
+
+    Returns:
+      gac_array (numpy.ndtype): array with scan width of 400 pixels
+
+    Note:
+      Original GAC data contains 401 pixels per scanline, for the sake
+      of simplicity we take only 400 pixels.
+
+    """
+    window_size = 5
+    lac_array_with_omitted_lines = lac_array[::4]
+    lac_array_2000px = lac_array_with_omitted_lines[:,:-1]
+    flat_lac_array = lac_array_2000px.flatten()
+    gac_array_flat = np.mean(window_blocks(flat_lac_array, window_size)[:,:-1], axis=1)
+    gac_length = gac_array_flat.shape[0]
+    gac_array_2d = gac_array_flat.reshape(gac_length/400, 400)
+    return gac_array_2d
