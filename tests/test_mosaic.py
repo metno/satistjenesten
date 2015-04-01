0b9399f4cf6b5ca87e8b3d40f9bd2ab85f2f7ad8
Mikhail Itkin <itkin.m@gmail.com>
2015-04-01 09:24:19 +0200

    [mosaic] use osisaf netcdf for tests * and don't save rgb image, that is not a proper part of the test

--- tests/test_mosaic.py
+++ tests/test_mosaic.py
@@ -1,22 +1,23 @@
 import unittest
+import os
 from satistjenesten.mosaic import MosaicScene
-from satistjenesten.io import load_mitiff
+from satistjenesten.io import load_osisaf_amsr2_netcdf
 
 class TestMosaicScene(unittest.TestCase):
     def setUp(self):
-        self.bands_list = [4, 3, 2]
-        self.mitiff1 = load_mitiff('test_data/noaa201503111534msv.mitiff', bands=self.bands_list)
-        self.mitiff2 = load_mitiff('test_data/noaa201503102141msv.mitiff', bands=self.bands_list)
+        self.bands_list = ['lat_h']
+        self.scene_1 = load_osisaf_amsr2_netcdf('test_data/amsr2.nc', bands=self.bands_list)
+        self.scene_2 = load_osisaf_amsr2_netcdf('test_data/amsr2.nc', bands=self.bands_list)
         self.mosaic = MosaicScene()
-        self.mosaic.get_area_def(area_name='istjenesten_main_1k')
+        self.mosaic.get_area_def(area_name='nsidc_stere_north_300k')
 
     def test_MosaicScene_AddsScenes(self):
-        self.mosaic.add_scenes([self.mitiff1])
-        self.assertEquals(self.mosaic.start_timestamp, self.mitiff1.timestamp)
-        self.assertNotEquals(self.mosaic.area_def, self.mitiff1.area_def)
+        self.mosaic.add_scenes([self.scene_1])
+        self.assertEquals(self.mosaic.start_timestamp, self.scene_1.timestamp)
+        self.assertNotEquals(self.mosaic.area_def, self.scene_1.area_def)
 
     def test_MosaicScene_ComposeMosaic(self):
-        self.mosaic.add_scenes([self.mitiff1, self.mitiff2])
+        self.mosaic.add_scenes([self.scene_1, self.scene_2])
         self.mosaic.compose_mosaic()
         self.mosaic.save_geotiff('out.tif', bands=self.bands_list)
-        self.mosaic.save_rgb_image('rgb.png', self.bands_list)
+        self.assertTrue(os.path.exists('out.tif'))
