cc0e92e7638c3b967ee2c49c780794ba7c585a0d
Mikhail Itkin <itkin.m@gmail.com>
2015-06-24 14:55:35 +0200

    [behave] added environment.py
    Control debug, summon ipdb post-mortem if
    feature fails

---
+++ features/environment.py
@@ -0,0 +1,15 @@
+BEHAVE_DEBUG_ON_ERROR = False
+
+def setup_debug_on_error(userdata):
+    global BEHAVE_DEBUG_ON_ERROR
+    BEHAVE_DEBUG_ON_ERROR = userdata.getbool("BEHAVE_DEBUG_ON_ERROR")
+
+def before_all(context):
+    setup_debug_on_error(context.config.userdata)
+
+def after_step(context, step):
+    if BEHAVE_DEBUG_ON_ERROR and step.status == "failed":
+        # -- ENTER DEBUGGER: Zoom in on failure location.
+        # NOTE: Use IPython debugger, same for pdb (basic python debugger).
+        import ipdb
+        ipdb.post_mortem(step.exc_traceback)
